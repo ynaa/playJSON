133da54eadfa66905ac71838483997bc46ee601b
Yngve <yngve.aas@gmail.com>
2015-10-14 23:13:05 +0200

    new build-system

--- public/react/src/app.js
+++ public/react/src/app.js
@@ -1,7 +1,9 @@
 define(['react', 'menu', 'content'], function(React, Menu, Content){
 	var App = React.createClass({
 			render: function() {
-					return <div><Menu/><div id='content'><Content/></div></div>;
+					return <div><Menu/><div id='content'>
+					
+					</div></div>;
 			}
 	});
 
